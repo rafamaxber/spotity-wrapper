4f621ba273cef0e8a55bc833d9a880cd29628c91
Rafael Bernardo <rafamaxber@gmail.com>
2017-05-11 01:28:45 -0300

    intial

---
+++ src/main.js
@@ -0,0 +1,19 @@
+function search() {
+  return fetch('https://google.com');
+}
+function searchAlbum() {
+
+}
+function searchArtist() {
+
+}
+function searchPlaylist() {
+
+}
+function searchTrack() {
+
+}
+
+export {
+  search, searchAlbum, searchArtist, searchPlaylist, searchTrack
+};
--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,5 +1,51 @@
-import { expect } from 'chai';
+import chai, { expect } from 'chai';
 
-describe('Main', () => {
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+import sinonStubPromise from 'sinon-stub-promise';
+
+chai.use(sinonChai);
+sinonStubPromise(sinon);
+
+global.fetch = require('node-fetch');
+
+import { search, searchAlbum, searchArtist, searchPlaylist, searchTrack } from '../src/main.js';
+
+describe('Spotify Wrapper', () => {
+
+  describe('Smoke tests', () => {
+
+    it('Should exist the search method', () => {
+      expect(search).to.exist;
+    });
+
+    it('Should exist the searchAlbum method', () => {
+      expect(searchAlbum).to.exist;
+    });
+
+    it('Should exist the searchArtist method', () => {
+      expect(searchArtist).to.exist;
+    });
+
+    it('Should exist the searchTrack method', () => {
+      expect(searchTrack).to.exist;
+    });
+
+    it('Should exist the searchPlaylist method', () => {
+      expect(searchPlaylist).to.exist;
+    });
+
+   });
+
+   describe('Generic search', () => {
+
+    it('Should call fetch function', () => {
+      const fetchStub = sinon.stub(global, 'fetch');
+      const artists = search();
+
+      expect(fetchStub).to.have.been.calledOnce;
+    });
+
+   });
 
 });
