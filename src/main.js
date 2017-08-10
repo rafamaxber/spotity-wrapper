a766d4ec3ba6684bfd146f9cb874503fe0747bcb
Rafael Bernardo <rafamaxber@gmail.com>
2017-08-10 09:23:11 -0300

    add eslint-plugin-chai

--- src/main.js
+++ src/main.js
@@ -1,18 +1,18 @@
 function search(query, type) {
   return fetch(`https://api.spotify.com/v1/search?q=${query}&type=${type}`)
-  .then(data => data.json());
+    .then(data => data.json());
 }
 function searchArtist(query) {
-  return search(query, 'artist')
+  return search(query, 'artist');
 }
 function searchAlbum(query) {
-  return search(query, 'album')
+  return search(query, 'album');
 }
 function searchPlaylist(query) {
-  return search(query, 'playlist')
+  return search(query, 'playlist');
 }
 function searchTrack(query) {
-  return search(query, 'track')
+  return search(query, 'track');
 }
 
 export {
